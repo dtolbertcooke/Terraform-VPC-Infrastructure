421c4b964f29b455acb4a9682dc550d394e959d1
Doug T. <dtolbertcooke@gmail.com>
2025-11-13 02:14:59 -0500

    formatting

--- infrastructure/modules/vpc/variables.tf
+++ infrastructure/modules/vpc/variables.tf
@@ -38,9 +38,9 @@ variable "public_subnets" {
   description = "CIDR blocks for public subnets"
   type        = list(string)
   default     = ["10.0.0.0/24", "10.0.1.0/24"]
-  
+
   validation {
-    condition = length(var.public_subnets) >= 2
+    condition     = length(var.public_subnets) >= 2
     error_message = "At least 2 public subnets are required for high availability."
   }
 }
@@ -49,9 +49,9 @@ variable "private_subnets" {
   description = "CIDR blocks for private subnets"
   type        = list(string)
   default     = ["10.0.2.0/24", "10.0.3.0/24"]
-  
+
   validation {
-    condition = length(var.private_subnets) >= 2
+    condition     = length(var.private_subnets) >= 2
     error_message = "At least 2 private subnets are required for high availability."
   }
 }
