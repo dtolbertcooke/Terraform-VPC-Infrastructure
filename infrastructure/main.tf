d936050aac69aba40660f7da99e2905a05661f2f
Doug T. <dtolbertcooke@gmail.com>
2025-11-13 03:17:50 -0500

    updating main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,11 +3,13 @@ locals {
 }
 
 module "vpc" {
-  source       = "./modules/vpc"
-  project_name = local.config.project_name
-  environment  = var.environment
-  vpc_cidr     = local.config.vpc_cidr
-  azs          = local.config.azs
-  owner        = local.config.owner
-  personal_ip  = var.personal_ip
+  source          = "./modules/vpc"
+  project_name    = local.config.project_name
+  environment     = var.environment
+  vpc_cidr        = local.config.vpc_cidr
+  azs             = local.config.azs
+  public_subnets  = local.config.public_subnets
+  private_subnets = local.config.private_subnets
+  owner           = local.config.owner
+  personal_ip     = var.personal_ip
 }
